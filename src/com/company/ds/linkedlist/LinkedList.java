832680bf170d518cbc0fe6228b8995b402efb04d
jishan ansari <ansari_jishan@network.lilly.com>
2022-04-11 22:47:19 +0530

    added insert function to linked list

--- src/com/company/ds/linkedlist/LinkedList.java
+++ src/com/company/ds/linkedlist/LinkedList.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LinkedList {
-
-    Node head ;
-    Node tail ;
+    Node head;
+    Node tail;
     int length = 0;
 
-    LinkedList(Integer value){
+    LinkedList(Integer value) {
         Node node = new Node();
         node.data = value;
         node.nextNode = null;
@@ -18,7 +17,13 @@ public class LinkedList {
         length++;
     }
 
-    public void append(Integer value){
+    /**
+     * Function to append the node to the linkedList
+     * time complexity is O(1)
+     *
+     * @param value
+     */
+    public void append(Integer value) {
         //created new node
         Node newNode = new Node();
         newNode.data = value;
@@ -30,7 +35,13 @@ public class LinkedList {
         length++;
     }
 
-    public void preAppend(Integer value){
+    /**
+     * Function to pre-append the node to the linkedList
+     * time complexity is O(1)
+     *
+     * @param value
+     */
+    public void preAppend(Integer value) {
         //create new node
         Node newNode = new Node();
         newNode.data = value;
@@ -40,19 +51,75 @@ public class LinkedList {
         length++;
     }
 
+    /**
+     * Function to insert in between two nodes of linked list
+     * @param index given index against which user want to enter the new node
+     * @param value int value for new node
+     * @return Node [Node]
+     */
+    public Node insert(int index, int value) {
 
-    public List<Node> printAllNode(){
+        // create new node with given value
+        Node newNode = new Node();
+        newNode.data = value;
+        newNode.nextNode = null;
+
+        int i = 0;
+        Node currentNode = head;
+        while (currentNode != null) {
+            if(i == index-1){
+                newNode.nextNode = currentNode.nextNode;
+                currentNode.nextNode = newNode;
+            }
+            currentNode = currentNode.nextNode;
+            i++;
+        }
+
+        return newNode;
+    }
+
+
+    /**
+     * Function to print all the nodes of linked list
+     * time complexity is O(n)
+     *
+     * @return List<Node>
+     */
+    public List<Node> printAllNode() {
         ArrayList<Node> nodes = new ArrayList<>();
         Node givenNode = head;
-        while (givenNode != null){
+        while (givenNode != null) {
             nodes.add(givenNode);
             givenNode = givenNode.nextNode;
         }
         return nodes;
     }
 
+    /**
+     * Function to get size of linked list
+     *
+     * @return
+     */
     public int getLength() {
         return length;
+    }
+
+    /**
+     * Function to get head of arraylist
+     *
+     * @return
+     */
+    public Node getHead() {
+        return head;
+    }
+
+    /**
+     * Function to get tail of arraylist
+     *
+     * @return
+     */
+    public Node getTail() {
+        return tail;
     }
 
     @Override
--- src/com/company/ds/linkedlist/Main.java
+++ src/com/company/ds/linkedlist/Main.java
@@ -1,11 +1,13 @@
 package com.company.ds.linkedlist;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList(10);
+        linkedList.append(82);
         linkedList.append(12);
         linkedList.append(14);
         linkedList.append(16);
@@ -13,13 +15,24 @@ public class Main {
         linkedList.preAppend(8);
         linkedList.preAppend(6);
 
+        //inserting in middle into given index
+        linkedList.insert(2,5);
+        linkedList.insert(5,40);
+        linkedList.insert(5,100);
+
         System.out.println(linkedList);
 
-        System.out.println("Size of this Linked list is : "+linkedList.getLength());
+        System.out.println("Size of this Linked list is : " + linkedList.getLength());
+        System.out.println("Head of Linked list is : " + linkedList.getHead().data);
+        System.out.println("Tail of Linked list is : " + linkedList.getTail().data);
 
-        List<Node> nodes=linkedList.printAllNode();
-        for(Node data : nodes){
-            System.out.println(data.data);
+        List<Node> nodes = linkedList.printAllNode();
+        for (Node data : nodes) {
+            if (data.nextNode != null) {
+                System.out.println("Current Node data : " + data.data + " Next Node data :" + data.nextNode.data);
+            } else {
+                System.out.println("Current Node data : " + data.data);
+            }
         }
     }
 
