2ec3bee35ca15498f330bb804e41b3bd1fffb01a
jishan ansari <ansari_jishan@network.lilly.com>
2022-04-22 15:09:21 +0530

    bubble sort program refactored

--- src/com/company/algorithms/recurssion/sorting/BubbleSort.java
+++ src/com/company/algorithms/recurssion/sorting/BubbleSort.java
@@ -5,12 +5,15 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] numbers = {7, 5, 9, 6, 4, 8, 3, 2};
+        int[] numbers = {7, 5, 9, 6, 4, 8, 3, 2, 44, 678, 4, 33, 234, 89, 67, 39, 93, 100};
         int[] sortedNumbers = bubbleSort(numbers);
         System.out.println(Arrays.toString(sortedNumbers));
     }
 
     public static int[] bubbleSort(int[] numbers) {
+        if (numbers.length <= 1) {
+            return numbers;
+        }
         for (int i = numbers.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 //{3,7,4,3,9,4}  here if number[5] is less than number[4] then swap
@@ -19,7 +22,6 @@ public class BubbleSort {
                 }
             }
         }
-
         return numbers;
     }
 
